40ac9291aaa2a8f2ebc3137d912dea8d27480ee8
Jessica <jessijob@rambler.ru>
2024-05-17 15:06:18 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { Chart } from "react-google-charts";
 
 function App() {
+  const data = [
+    ["Задача", "Часов  в день"],
+    ["Работа", 8],
+    ["Еда", 2],
+    ["Сон", 2],
+    ["Обучение", 3],
+    ["Сон", 7],
+    ["Компьютерные игры", 2]
+  ];
+
+  const options = {
+    title: "Моя дневная активность",
+    is3D: true,
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pie Chart график</h1>
+      <Chart
+        chartType="PieChart"
+        data={data}
+        options={options}
+        width="100%"
+        height="400px"
+        legendToggle
+      />
     </div>
   );
 }
 
 export default App;
+
